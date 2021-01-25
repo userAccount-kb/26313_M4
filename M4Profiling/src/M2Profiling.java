32d528d1f83c12b6980d90ecd92e661c283ceee3
Kashai <sweetpea617@outlook.com>
2021-01-24 22:57:16 -0500

    code tested - one change to makes all lists and table size equal 2M and not 2M+1; pseudocode added to the instructions.txt file; screenshots added to main folder of functionality

--- M4Profiling/src/M2Profiling.java
+++ M4Profiling/src/M2Profiling.java
@@ -1,30 +1,35 @@
 import java.util.*;
 
+/**
+ * Calls 3 methods that will add 2 million random integers into an
+ * ArrayList, LinkedList, and Hashtable respectively which then are deleted.
+ */
 public class M2Profiling {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
         /*Call a new method which adds 2,000,000 random integers into an ArrayList, then 
         deletes each one from the ArrayList*/
-        System.out.println("Beginning the ArrayList method");
         randArrayList();
 
         /*Call a new method which adds 2,000,000 random integers into a LinkedList, 
         then deletes each one from the LinkedList*/
-        System.out.println("Beginning the LinkedList method");
         randLinkedList();
 
         /*Call a new method which adds 2,000,000 random integers into a Hashtable, then 
         deletes each one from the Hashtable*/
-        System.out.println("Beginning the Hashtable method");
         randHastable();
     }
 
+    /**
+     * Creates, confirms, and then erases 2 million integers from an ArrayList.
+     */
     public static void randArrayList() {
+        System.out.println("Beginning the ArrayList method");
+
         //add 2M random int to an ArrayList
         int randNum;
         ArrayList<Integer> randArray = new ArrayList<>();
 
-        for (int i = 0; i <= 2000000; i++) {
+        for (int i = 0; i < 2000000; i++) {
             randNum = (int)Math.random();
             randArray.add(randNum);
         }
@@ -37,12 +42,17 @@ public class M2Profiling {
         System.out.println("Hastable is clear: " + randArray.isEmpty());
     }
 
+    /**
+     * Creates, confirms, and then erases 2 million integers from a LinkedList.
+     */
     public static void randLinkedList() {
+        System.out.println("\nBeginning the LinkedList method");
+
         //add 2M random int to an LinkedList
         int randNum;
         LinkedList<Integer> randList = new LinkedList<>();
 
-        for (int i = 0; i <= 2000000; i++) {
+        for (int i = 0; i < 2000000; i++) {
             randNum = (int)Math.random();
             randList.add(randNum);
         }
@@ -55,12 +65,17 @@ public class M2Profiling {
         System.out.println("Hastable is clear: " + randList.isEmpty());
     }
 
+    /**
+     * Creates, confirms, and then erases 2 million integers from a Hashtable.
+     */
     public static void randHastable() {
+        System.out.println("\nBeginning the Hashtable method");
+
         //add 2M random int to an Hastable
         int randNum;
         Hashtable<Integer, Integer> randHashtable = new Hashtable<>();
 
-        for (int i = 0; i <= 2000000; i++) {
+        for (int i = 0; i < 2000000; i++) {
             randNum = (int)Math.random();
             randHashtable.put(i, randNum);
         }
